c282df5924140a2fc0bbb71dd3d82bdca4db0b44
S Stewart <tilda@users.noreply.github.com>
2020-05-06 01:40:31 -0500

    did you know: i never tested this lib ever

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use winreg::enums::*;
 use winreg::RegKey;
 
 #[cfg(target_os = "win32")]
-pub fn getID() -> std::string::String {
+pub fn get_id() -> std::string::String {
     // escaping is fun, right? right???
     let hive = winreg::RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey("\\\\SOFTWARE\\Microsoft\\Cryptography").expect("Failed to open subkey");
     let id = hive.get_value("MachineGuid").expect("Failed to get MachineGuid");
@@ -17,14 +17,14 @@ pub fn getID() -> std::string::String {
 }
 
 #[cfg(target_os = "darwin")]
-pub fn getID() -> std::string::String {
+pub fn get_id() -> std::string::String {
     let cmd = std::process::Command::new("ioreg").arg("-rd1").arg("-c").arg("IOExpertPlatformDevice").output().expect("Failed to get HWID");
     let id = cmd.stdout.last();
     return id
 }
 
 #[cfg(target_os = "linux")]
-pub fn getID() -> std::string::String {
+pub fn get_id() -> std::string::String {
     let mut id = Path::new("/var/lib/dbus/machine-id").exists();
     if id == false {
         id = Path::new("/etc/machine-id").exists();
@@ -40,6 +40,6 @@ pub fn getID() -> std::string::String {
 #[cfg(target_os = "dragonfly")]
 #[cfg(target_os = "openbsd")]
 #[cfg(target_os = "netbsd")]
-pub fn getID() -> std::string::String {
+pub fn get_id() -> std::string::String {
     panic!("*BSD support is not implemented")
 }
