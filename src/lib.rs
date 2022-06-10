2c723470b1c4af70751885cd9dc1da0654728c13
Bart Massey <bart.massey@gmail.com>
2022-06-09 17:12:25 -0700

    cleaned up API including proper error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,68 @@
 // rust-hwid
 // (c) 2020 tilda, under MIT license
 
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum HwIdError {
+    #[error("no HWID was found on system")]
+    NotFound,
+    #[error("{0:?}: contains malformed HWID")]
+    Malformed(String),
+}
+
 #[cfg(target_os = "windows")]
 mod hwid {
-
-    extern crate winreg;
+    use super::*;
     use winreg::enums::HKEY_LOCAL_MACHINE;
 
-    pub fn get_id() -> std::string::String {
+    pub fn get_id() -> Result<std::string::String, HwIdError> {
         // escaping is fun, right? right???
-        let hive = winreg::RegKey::predef(HKEY_LOCAL_MACHINE).open_subkey("\\\\SOFTWARE\\Microsoft\\Cryptography").expect("Failed to open subkey");
-        let id = hive.get_value("MachineGuid").expect("Failed to get MachineGuid");
-        return id
+        let hive = winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
+            .open_subkey(r"\\SOFTWARE\Microsoft\Cryptography")
+            .or(Err(HwIdError::NotFound))?;
+        let id = hive
+            .get_value("MachineGuid")
+            .or(Err(HwIdError::NotFound))?;
+        Ok(id)
     }
 }
 
 #[cfg(target_os = "darwin")]
 mod hwid {
-    pub fn get_id() -> std::string::String {
-        let cmd = std::process::Command::new("ioreg").arg("-rd1").arg("-c").arg("IOExpertPlatformDevice").output().expect("Failed to get HWID");
+    use super::*;
+
+    pub fn get_id() -> Result<std::string::String, HwIdError> {
+        let cmd = std::process::Command::new("ioreg")
+            .arg("-rd1")
+            .arg("-c")
+            .arg("IOExpertPlatformDevice")
+            .output()
+            .or(Err(HwIdError::NotFound))?;
         let id = cmd.stdout.last();
-        return id
+        Ok(id)
     }
 }
 
 #[cfg(target_os = "linux")]
 mod hwid {
-    pub fn get_id() -> std::string::String {
+    use super::*;
+
+    pub fn get_id() -> Result<std::string::String, HwIdError> {
         let paths = [
             "/var/lib/dbus/machine-id",
             "/etc/machine-id",
         ];
         for p in paths {
-            if let Ok(id_string) = std::fs::read_to_string(p) {
-                return id_string
+            if let Ok(id_contents) = std::fs::read_to_string(p) {
+                let id_str = id_contents
                     .lines()
                     .next()
-                    .expect("{p}: malformed id file")
-                    .to_string()
+                    .ok_or_else(|| HwIdError::Malformed(id_contents.to_string()))?;
+                return Ok(id_str.to_string());
             }
         }
-        panic!("No HWID was found on system");
+        Err(HwIdError::NotFound)
     }
 }
 
