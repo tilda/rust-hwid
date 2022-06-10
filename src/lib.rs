5eff0d00d77b13ecdfa855f3e03646ae509dfa69
Bart Massey <bart.massey@gmail.com>
2022-06-09 17:28:03 -0700

    added rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 // rust-hwid
 // (c) 2020 tilda, under MIT license
 
+//! Get a "Hardware ID" for the host machine. This is a UUID
+//! which is intended to uniquely represent this entity.
+
 use thiserror::Error;
 
+/// Possible failure cases for [get_id()].
 #[derive(Debug, Error)]
 pub enum HwIdError {
+    /// Could not detect a hardware id. This might be caused
+    /// by a misconfigured system or by this feature not
+    /// being supported by the system or platform.
     #[error("no HWID was found on system")]
     NotFound,
+    /// Found a putative HWID, but something was wrong with
+    /// it.  The `String` argument contains a path or other
+    /// identifier at which the HWID was found. This will
+    /// usually indicate something is really wrong with the
+    /// system.
     #[error("{0:?}: contains malformed HWID")]
     Malformed(String),
 }
@@ -16,6 +28,9 @@ mod hwid {
     use super::*;
     use winreg::enums::HKEY_LOCAL_MACHINE;
 
+    /// Get the hardware ID of this machine. The HWID is
+    /// obtained from the Windows registry at location
+    /// `\\\\SOFTWARE\\Microsoft\\Cryptography\\MachineGuid`.
     pub fn get_id() -> Result<std::string::String, HwIdError> {
         // escaping is fun, right? right???
         let hive = winreg::RegKey::predef(HKEY_LOCAL_MACHINE)
@@ -32,6 +47,14 @@ mod hwid {
 mod hwid {
     use super::*;
 
+    /// Get the hardware ID of this machine. The HWID is
+    /// obtained by running
+    ///
+    /// ```text
+    /// ioreg -rd1 -c IOExpertPlatformDevice
+    /// ```
+    ///
+    /// and returning the result.
     pub fn get_id() -> Result<std::string::String, HwIdError> {
         let cmd = std::process::Command::new("ioreg")
             .arg("-rd1")
@@ -48,6 +71,9 @@ mod hwid {
 mod hwid {
     use super::*;
 
+    /// Get the hardware ID of this machine. The HWID is
+    /// obtained from `/var/lib/dbus/machine-id`, or failing
+    /// that from `/etc/machine-id`.
     pub fn get_id() -> Result<std::string::String, HwIdError> {
         let paths = [
             "/var/lib/dbus/machine-id",
